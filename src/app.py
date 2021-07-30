450234528d5cd94f4ecda164fa635c3f5b8515af
Aldo Matus <aldo.matus@outlook.com>
2021-07-30 16:42:34 -0500

    All REST APIs added

--- src/app.py
+++ src/app.py
@@ -1,18 +1,161 @@
+# Flask libraries
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+
+# Third-party libraries
 from sqlalchemy import ForeignKey
-import datetime
+from datetime import datetime
 
 # For seed script
 import csv
 
+#-------------------database connection------------
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db/shipping_01'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db/flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+
+#----------------Models-------------------------------
+# Address information model
+class RemitentAddress(db.Model):
+    __tablename__= 'remitentaddress'
+    id = db.Column(db.Integer, primary_key=True)
+    postal_code = db.Column(db.String(10))
+    state = db.Column(db.String(30), unique=False)
+    municipality = db.Column(db.String(100), unique=False)
+    city = db.Column(db.String(100), unique=False)
+    colony = db.Column(db.String(100), unique=False)
+
+    remitent_id = db.Column(db.Integer, db.ForeignKey('remitent.id'))
+
+    def __init__(self, postal_code, state, municipality, city, colony):
+        self.postal_code = postal_code
+        self.state = state
+        self.municipality = municipality
+        self.city = city
+        self.colony = colony
+
+class RemitentAddressSchema(ma.Schema):
+    class Meta:
+      fields = ("id", "postal_code", "state", "municipality", "city", "colony", "remitent_id")
+
+address_r_schema = RemitentAddressSchema()
+addresses_r_schema = RemitentAddressSchema(many=True)
+
+
+# Address information model
+class DestinataryAddress(db.Model):
+    __tablename__= 'destinataryaddress'
+    id = db.Column(db.Integer, primary_key=True)
+    postal_code = db.Column(db.String(10))
+    state = db.Column(db.String(30), unique=False)
+    municipality = db.Column(db.String(100), unique=False)
+    city = db.Column(db.String(100), unique=False)
+    colony = db.Column(db.String(100), unique=False)
+
+    remitent_id = db.Column(db.Integer, db.ForeignKey('destinatary.id'))
+
+    def __init__(self, postal_code, state, municipality, city, colony):
+        self.postal_code = postal_code
+        self.state = state
+        self.municipality = municipality
+        self.city = city
+        self.colony = colony
+
+class DestinataryAddressSchema(ma.Schema):
+    class Meta:
+      fields = ("id", "postal_code", "state", "municipality", "city", "colony", "remitent_id")
+
+address_d_schema = DestinataryAddressSchema()
+addresses_d_schema = DestinataryAddressSchema(many=True)
+
+# Shipping information model
+class Shipping(db.Model):
+    __tablename__= 'shipping'
+    id = db.Column(db.Integer, primary_key=True)
+    contents = db.Column(db.String(100), nullable=False, unique=False)
+    product_value = db.Column(db.Float)
+    description = db.Column(db.String(100))
+    delivered = db.Column(db.Boolean, default=0)
+    shipping_price = db.Column(db.Float, nullable=False)
+    shipping_date = db.Column(db.DateTime)
+    
+    remitent_id = db.Column(db.Integer, db.ForeignKey('remitent.id'))
+    destinatary_id = db.Column(db.Integer, db.ForeignKey('destinatary.id'))
+
+    def __init__(self, contents, product_value, description, delivered, shipping_price, shipping_date):
+        self.contents = contents
+        self.product_value = product_value
+        self.description = description
+        self.delivered = delivered
+        self.shipping_price = shipping_price
+        self.shipping_date = shipping_date
+
+class ShippingSchema(ma.Schema):
+    class Meta:
+        ields = ("id", "contents", "product_value", "description", "delivered", "shipping_price", "shipping_date", "remitent_id", "destinatary_id")
+
+shipping_schema = ShippingSchema()
+shippings_schema = ShippingSchema(many=True)
+    
+
+# Remitent information model
+class Remitent(db.Model):
+    __tablename__= 'remitent'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    last_name = db.Column(db.String(100), unique=False, nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+
+    shippings = db.relationship('Shipping')
+    address = db.relationship('RemitentAddress')
+
+    def __init__(self, name, last_name, phone):
+        self.name = name
+        self.last_name = last_name
+        self.phone = phone
+
+class RemitentSchema(ma.Schema):
+    class Meta:
+      fields = ("id", "name", "last_name", "phone")
+
+remitent_schema = RemitentSchema()
+remitents_schema = RemitentSchema(many=True)        
+
+
+# Destinatary information model
+class Destinatary(db.Model):
+    __tablename__= 'destinatary'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False)
+    last_name = db.Column(db.String(100), unique=False)
+    address_model_d = db.Column(db.String(100))
+    phone = db.Column(db.String(20), unique=False)
+    postal_code = db.Column(db.String(10))
+    
+    remitent_id = db.relationship('Shipping')
+    address = db.relationship('DestinataryAddress')
+
+    def __init__(self, name, last_name, address_model_d, phone, postal_code):
+        
+        self.name = name
+        self.last_name = last_name
+        self.address_model_d = address_model_d
+        self.phone = phone
+        self.postal_code = postal_code
+
+class DestinatarySchema(ma.Schema):
+    class Meta:
+       fields = ("id", "name", "last_name", "address_model_d", "phone", "postal_code")
+
+destinatary_schema = DestinatarySchema()
+destinataries_schema = DestinatarySchema(many=True)  
+
 
 # Address information model
 class Address(db.Model):
@@ -24,15 +167,13 @@ class Address(db.Model):
     city = db.Column(db.String(100), unique=False)
     colony = db.Column(db.String(100), unique=False)
 
-    remitent_id = db.Column(db.Integer, db.ForeignKey('remitent.delivery_number'))
-
     def __init__(self, postal_code, state, municipality, city, colony):
         self.postal_code = postal_code
         self.state = state
         self.municipality = municipality
         self.city = city
         self.colony = colony
-
+db.create_all()
 class AddressSchema(ma.Schema):
     class Meta:
       fields = ("id", "postal_code", "state", "municipality", "city", "colony")
@@ -40,65 +181,8 @@ class AddressSchema(ma.Schema):
 address_schema = AddressSchema()
 addresses_schema = AddressSchema(many=True)
 
-# Shipping information model
-class Shipping(db.Model):
-    __tablename__= 'shipping'
-    delivery_number = db.Column(db.Integer, primary_key=True)
-    contents = db.Column(db.String(100), nullable=False, unique=False)
-    product_value = db.Column(db.Float)
-    description = db.Column(db.String(100))
-    delivered = db.Column(db.Boolean, default=False)
-    shipping_price = db.Column(db.Float)
-    shipping_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    
-    remitent_id = db.Column(db.Integer, db.ForeignKey('remitent.delivery_number'))
 
-    def __init__(self, contents, product_value, description, delivered, shipping_price, shipping_date):
-        self.contents = contents
-        self.product_value = product_value
-        self.description = description
-        self.delivered = delivered
-        self.shipping_price = shipping_price
-        self.shipping_date = shipping_date
-
-# Remitent information model
-class Remitent(db.Model):
-    __tablename__= 'remitent'
-
-    delivery_number = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=False, nullable=False)
-    last_name = db.Column(db.String(100), unique=False, nullable=False)
-    phone = db.Column(db.String(20), unique=True, nullable=False)
-
-    shippings = db.relationship('Shipping')
-    address = db.relationship('Address')
-
-    def __init__(self, name, last_name, phone):
-        self.name = name
-        self.last_name = last_name
-        self.phone = phone
-        
-
-# Destinatary information model
-class Destinatary(db.Model):
-    __tablename__= 'destinatary'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=False)
-    last_name = db.Column(db.String(100), unique=False)
-    address = db.Column(db.String(100), unique=False)
-    phone = db.Column(db.String(20), unique=False)
-    postal_code = db.Column(db.String(10))
-    remitent_id = db.Column(db.Integer, db.ForeignKey('remitent.delivery_number'))
-
-    def __init__(self, name, last_name, address, phone, postal_code):
-        self.name = name
-        self.last_name = last_name
-        self.address = address
-        self.phone = phone
-        self.postal_code = postal_code
-
-db.create_all()
-
+#-----------------Upload the SEPOMEX files to the database---------------
 # Seed script
 def import_data(file):
     """
@@ -109,8 +193,6 @@ def import_data(file):
         line_counter = 0
         for row in csv_reader:
             if line_counter != 0:
-                if line_counter == 100:
-                    print('hola')
                 postal_code=row[0]
                 colony=row[1]
                 municipality=row[3],
@@ -124,7 +206,13 @@ def import_data(file):
 
 import_data('puebla.csv')
 
+#----------------REST APIS---------------------------
+# Welcome to Cura Deuda ® API
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Welcome to Cura Deuda ® API'})
 
+# To create news addresses
 @app.route('/address', methods=['POST'])
 def create_address():
     # Receive requests
@@ -135,72 +223,103 @@ def create_address():
         city = request.json['city']
         colony = request.json['colony']
 
-        new_address= Address(postal_code, state, municipality, city, colony)
+        new_address= RemitentAddress(postal_code, state, municipality, city, colony)
 
         db.session.add(new_address)
         db.session.commit()
 
         return address_schema.jsonify(new_address)
 
-    if request.method == 'GET':
-        
-        postal_code_colonies = Address.query.filter_by(username='peter').first()
-        postal_code = request.json['postal_code']
 
-@app.route('/address', methods=['POST', 'GET'])
+# Save a customer submission service to the database
+@app.route('/shippings', methods=['POST'])
 def create_shipping():
+
     # Receive requests
-    postal_code = request.json['postal_code']
-    state = request.json['state']
-    municipality = request.json['municipality']
-    city = request.json['city']
-    colony = request.json['colony']
+    if request.method == 'POST':
 
-    new_address= Address(postal_code, state, municipality, city, colony)
+        # shipping information
+        contents = request.json['contents']
+        product_value = request.json['product_value']
+        description = request.json['description']
+        delivered = request.json['delivered']
+        shipping_price = request.json['shipping_price']
+        shipping_date = datetime.today()
 
-    db.session.add(new_address)
-    db.session.commit()
+        new_shipping= Shipping(contents, product_value, description, delivered, shipping_price, shipping_date)
+        db.session.add(new_shipping)
+        
 
-    return address_schema.jsonify(new_address)
+        # Remitent information
+        name_r = request.json['name_r']
+        last_name_r = request.json['last_name_r']
+        phone_r = request.json['phone_r']
+
+        postal_code_r = request.json['postal_code_r']
+        state_r = request.json['state_r']
+        municipality_r = request.json['municipality_r']
+        city_r = request.json['city_r']
+        colony_r = request.json['colony_r']
+
+        new_address_r= RemitentAddress(postal_code_r, state_r, municipality_r, city_r, colony_r)
+        db.session.add(new_address_r)
+
+        new_remitent = Remitent(name_r, last_name_r, phone_r)
+        db.session.add(new_remitent)
+
+        # Destinatary information
+        name_d = request.json['name_d']
+        last_name_d = request.json['last_name_d']
+        address_model_d = request.json['address_model_d']
+        phone_d = request.json['phone_d']
+        postal_code_d = request.json['postal_code_d']
+
+        postal_code_d = request.json['postal_code_d']
+        state_d = request.json['state_d']
+        municipality_d = request.json['municipality_d']
+        city_d = request.json['city_d']
+        colony_d = request.json['colony_d']
+        
+        new_address_d= DestinataryAddress(postal_code_d, state_d, municipality_d, city_d, colony_d)
+        db.session.add(new_address_d)
+
+        new_destinatary= Destinatary(name_d, last_name_d, address_model_d, phone_d, postal_code_d)
+        db.session.add(new_destinatary)
+
+        db.session.commit()
+        return destinatary_schema.jsonify(new_destinatary)
 
 
-# /--------------------------------------------------------
-# @app.route('/tasks', methods=['GET'])
-# def get_tasks():
-#   all_tasks = Task.query.all()
-#   result = tasks_schema.dump(all_tasks)
-#   return jsonify(result)
-
-# @app.route('/tasks/<id>', methods=['GET'])
-# def get_task(id):
-#   task = Task.query.get(id)
-#   return task_schema.jsonify(task)
-
-# @app.route('/tasks/<id>', methods=['PUT'])
-# def update_task(id):
-#   task = Task.query.get(id)
-
-#   title = request.json['title']
-#   description = request.json['description']
-
-#   task.title = title
-#   task.description = description
-
-#   db.session.commit()
-
-#   return task_schema.jsonify(task)
-
-# @app.route('/tasks/<id>', methods=['DELETE'])
-# def delete_task(id):
-#   task = Task.query.get(id)
-#   db.session.delete(task)
-#   db.session.commit()
-#   return task_schema.jsonify(task)
+    return jsonify({'message': 'Data stored successfully!'})
 
 
-# @app.route('/', methods=['GET'])
-# def index():
-#     return jsonify({'message': 'Welcome to my API'})
+# Search for colonies by zip code
+@app.route('/address/cp/<string:postal_code>', methods=['GET'])
+def get_colonies(postal_code):
+    colonies = Address.query.filter_by(postal_code=postal_code).all()
+    result = addresses_schema.dump(colonies)
+    return jsonify(result)
+
+# search for colonies, municipalities and states by name
+@app.route('/address/colonies/<string:place>', methods=['GET'])
+def get_colonies_name(place):
+    colonies = Address.query.filter_by(colony=place).all()
+    result = addresses_schema.dump(colonies)
+    return jsonify(result)
+
+# search for municipalities by name
+@app.route('/address/municipalities/<string:place>', methods=['GET'])
+def get_municipalities_name(place):
+    municipalities = Address.query.filter_by(municipality=place).all()
+    result = addresses_schema.dump(municipalities)
+    return jsonify(result)
+
+# search for states by name
+@app.route('/address/state/<string:place>', methods=['GET'])
+def get_state_name(place):
+    states = Address.query.filter_by(state=place).all()
+    result = addresses_schema.dump(states)
+    return jsonify(result)
 
 
 if __name__ == "__main__":
